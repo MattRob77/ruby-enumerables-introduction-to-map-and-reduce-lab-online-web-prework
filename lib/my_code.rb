fb7aeec118b4d4180762b569066889aa305fdc79
Matthew Robinson <matt_robinson1@yahoo.com>
2019-08-26 01:20:02 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -48,17 +48,7 @@ def reduce_to_any_true(source_array)
   end 
 end 
 
-def reduce_to_any_true(source_array)
-  counter = 0 
-  while counter < source_array.size do 
-    if source_array[counter] 
-      counter += 1 
-    else 
-      return true  
-    end 
-    false 
-  end 
-end 
+
 
 
 
